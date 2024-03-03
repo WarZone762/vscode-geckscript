a2d96b678cc6c15b14840e54a0ba5becabef6f8f
WarZone762 <uzi1m1@gmail.com>
2024-03-03 14:13:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# vscode-geckscript
+
+LSP setup for [geckscript-lsp](https://github.com/WarZone762/geckscript-lsp) and highlighting for geckscript for VSCode
+
+## Installation
+
+Get it on the [Visual Studio Code marketplace](https://marketplace.visualstudio.com/items?itemName=warzone762.geckscript)
---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+import { ExtensionContext, Uri } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const serverModule = Uri.joinPath(
+    context.extensionUri,
+    "node_modules",
+    "geckscript-lsp",
+    "lib",
+    "server.js",
+  ).fsPath;
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      args: ["--tree-view-server"],
+      options: debugOptions,
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "geckscript" }],
+  };
+
+  client = new LanguageClient(
+    "geckscriptServer",
+    "geckscript Server",
+    serverOptions,
+    clientOptions,
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
